95e40357ce9836ab931e92016f99650645460a0a
xgmak94 <xgmak94@gmail.com>
2022-08-01 10:33:37 -0700

    Getting ready for deploying

--- server/load.js
+++ server/load.js
@@ -2,37 +2,55 @@ import http from 'k6/http';
 import { check, sleep, group } from 'k6';
 
 export const options = {
-    vus: 10,
+    vus: 1000,
     duration: '30s',
 };
 
 export default function test() {
-  // group('GET products', () => {
-  //   const productTest = http.get(`http://localhost:3000/products`);
-  //   check(productTest, { 'status was 200': (r) => r.status === 200 });
-  //   sleep(1);
-  // });
+  group('GET products', () => {
+    const productTest = http.get(`http://localhost:3000/products`);
+    check(productTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  });
 
-  // group('GET products', () => {
-  //   const productTest = http.get(`http://localhost:3000/products?page=200000`);
-  //   check(productTest, { 'status was 200': (r) => r.status === 200 });
-  //   sleep(1);
-  // });
+  group('GET products', () => {
+    const productTest = http.get(`http://localhost:3000/products?page=200000`);
+    check(productTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  });
 
-  // group('GET specific product info', () => {
-  //   const infoTest = http.get(`http://localhost:3000/products/?product_id=1`);
-  //   check(infoTest, { 'status was 200': (r) => r.status === 200 });
-  //   sleep(1);
-  // })
+  group('GET specific product info', () => {
+    const infoTest = http.get(`http://localhost:3000/products/?product_id=1`);
+    check(infoTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  })
 
-  // group('GET specific product info', () => {
-  //   const infoTest = http.get(`http://localhost:3000/products/?product_id=${Math.floor(Math.random() * (1000000)) + 1}`);
-  //   check(infoTest, { 'status was 200': (r) => r.status === 200 });
-  //   sleep(1);
-  // })
+  group('GET specific product info', () => {
+    const infoTest = http.get(`http://localhost:3000/products/?product_id=${Math.floor(Math.random() * (1000000)) + 1}`);
+    check(infoTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  })
 
   group('GET product style info', () => {
-    const styleTest = http.get(`http://localhost:3000/products/?product_id=${Math.floor(Math.random() * (1000000)) + 1}`);
+    const styleTest = http.get(`http://localhost:3000/products/1/styles`);
+    check(styleTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  })
+
+  group('GET product style info', () => {
+    const styleTest = http.get(`http://localhost:3000/products/?product_id=${Math.floor(Math.random() * (1000000)) + 1}/styles`);
+    check(styleTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  })
+
+  group('GET related info', () => {
+    const styleTest = http.get(`http://localhost:3000/products/1/styles`);
+    check(styleTest, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+  })
+
+  group('GET related info', () => {
+    const styleTest = http.get(`http://localhost:3000/products/${Math.floor(Math.random() * (1000000)) + 1}/styles`);
     check(styleTest, { 'status was 200': (r) => r.status === 200 });
     sleep(1);
   })
